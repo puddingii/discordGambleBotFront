d6a3bbf2fb464b1a0e752e740929cc00c300ddc5
puddingii <gun4930@naver.com>
2023-03-26 00:06:31 +0900

    chore(query): 로그인 만료시 처리 추가

--- src/quires/useUserMutation.js
+++ src/quires/useUserMutation.js
@@ -1,5 +1,7 @@
 import axios from 'axios';
 import { useMutation, useQueryClient } from 'react-query';
+import { isLoggedIn, myNickname } from 'recoils/user';
+import { useSetRecoilState } from 'recoil';
 import { QUERY_KEY as userQueryKey } from './useUserQuery';
 // useMutation에서 사용할 `서버에 Side Effect를 발생시키기 위해 사용할 함수`
 // 이 함수의 파라미터로는 useMutation의 `mutate` 함수의 파라미터가 전달됩니다.
@@ -9,9 +11,17 @@ const giveMoneyFetcher = contents =>
 
 export const useGiveMoneyMutation = () => {
 	const queryClient = useQueryClient();
+	const setMyNickname = useSetRecoilState(myNickname);
+	const setIsLoggedIn = useSetRecoilState(isLoggedIn);
 	return useMutation(giveMoneyFetcher, {
 		onSuccess: () => {
 			queryClient.invalidateQueries(userQueryKey.giveMoney);
+		},
+		onError: e => {
+			if (e?.response?.status === 403) {
+				setMyNickname(() => '');
+				setIsLoggedIn(() => false);
+			}
 		},
 	});
 };
@@ -21,11 +31,22 @@ const getGrantMoneyFetch = () =>
 
 export const useGetGrantMoneyMutation = () => {
 	const queryClient = useQueryClient();
+	const setMyNickname = useSetRecoilState(myNickname);
+	const setIsLoggedIn = useSetRecoilState(isLoggedIn);
 	return useMutation(getGrantMoneyFetch, {
 		onSuccess: () => {
 			queryClient.invalidateQueries(userQueryKey.getGrantMoney);
 		},
+		onError: e => {
+			if (e?.response?.status === 403) {
+				setMyNickname(() => '');
+				setIsLoggedIn(() => false);
+			}
+		},
 	});
 };
 
-export default { useGiveMoneyMutation, useGetGrantMoneyMutation };
+export default {
+	useGiveMoneyMutation,
+	useGetGrantMoneyMutation,
+};
--- src/quires/useUserQuery.js
+++ src/quires/useUserQuery.js
@@ -1,6 +1,8 @@
 // quires/useTodosQuery.ts
 import axios from 'axios';
 import { useQuery } from 'react-query';
+import { isLoggedIn, myNickname } from 'recoils/user';
+import { useSetRecoilState } from 'recoil';
 // useQuery에서 사용할 UniqueKey를 상수로 선언하고 export로 외부에 노출합니다.
 // 상수로 UniqueKey를 관리할 경우 다른 컴포넌트 (or Custom Hook)에서 쉽게 참조가 가능합니다.
 export const QUERY_KEY = {
@@ -10,7 +12,7 @@ export const QUERY_KEY = {
 	myStock: `${process.env.REACT_APP_BACK_API}/user/stock?`,
 	updateStock: `${process.env.REACT_APP_BACK_API}/user/stock?`,
 	userNicknameList: `${process.env.REACT_APP_BACK_API}/user/nicklist?`,
-	giveMoney: `${process.env.REACT_APP_BACK_API}/user/give/money`,
+	giveMoney: `${process.env.REACT_APP_BACK_API}/user/give-money`,
 	getGrantMoney: `${process.env.REACT_APP_BACK_API}/user/grantmoney`,
 };
 
@@ -18,20 +20,48 @@ export const QUERY_KEY = {
 const getUserInfoFetcher = () =>
 	axios.get(QUERY_KEY.get, { withCredentials: true }).then(({ data }) => data);
 export const useGetUserInfoQuery = () => {
-	return useQuery(QUERY_KEY.get, getUserInfoFetcher, { refetchOnWindowFocus: false });
+	const setMyNickname = useSetRecoilState(myNickname);
+	const setIsLoggedIn = useSetRecoilState(isLoggedIn);
+	return useQuery(QUERY_KEY.get, getUserInfoFetcher, {
+		refetchOnWindowFocus: false,
+		onError: e => {
+			if (e?.response?.status === 403) {
+				setMyNickname(() => '');
+				setIsLoggedIn(() => false);
+			}
+		},
+	});
 };
 
 const getMyStockList = () =>
 	axios.get(QUERY_KEY.myStockList, { withCredentials: true }).then(({ data }) => data);
 export const useGetUserStockListQuery = () => {
-	return useQuery(QUERY_KEY.myStockList, getMyStockList, { refetchOnWindowFocus: false });
+	const setMyNickname = useSetRecoilState(myNickname);
+	const setIsLoggedIn = useSetRecoilState(isLoggedIn);
+	return useQuery(QUERY_KEY.myStockList, getMyStockList, {
+		refetchOnWindowFocus: false,
+		onError: e => {
+			if (e?.response?.status === 403) {
+				setMyNickname(() => '');
+				setIsLoggedIn(() => false);
+			}
+		},
+	});
 };
 
 const getMyWeaponList = () =>
 	axios.get(QUERY_KEY.myWeaponList, { withCredentials: true }).then(({ data }) => data);
 export const useGetUserWeaponListQuery = () => {
+	const setMyNickname = useSetRecoilState(myNickname);
+	const setIsLoggedIn = useSetRecoilState(isLoggedIn);
 	return useQuery(QUERY_KEY.myWeaponList, getMyWeaponList, {
 		refetchOnWindowFocus: false,
+		onError: e => {
+			if (e?.response?.status === 403) {
+				setMyNickname(() => '');
+				setIsLoggedIn(() => false);
+			}
+		},
 	});
 };
 
@@ -40,8 +70,16 @@ const getUserNicknameList = () =>
 		.get(QUERY_KEY.userNicknameList, { withCredentials: true })
 		.then(({ data }) => data);
 export const useGetUserNicknameListQuery = () => {
+	const setMyNickname = useSetRecoilState(myNickname);
+	const setIsLoggedIn = useSetRecoilState(isLoggedIn);
 	return useQuery(QUERY_KEY.userNicknameList, getUserNicknameList, {
 		refetchOnWindowFocus: false,
+		onError: e => {
+			if (e?.response?.status === 403) {
+				setMyNickname(() => '');
+				setIsLoggedIn(() => false);
+			}
+		},
 	});
 };
 
